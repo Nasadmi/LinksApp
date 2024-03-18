fea3045658364f3722c79f47ca13b33874f363ed
Nasadmi <samueldiego828@gmail.com>
2024-03-18 22:46:59 +0100

    Update app and main files

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { join } from 'node:path';
+import { UsersModule } from './users/users.module';
+import { LinksModule } from './links/links.module';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/nest'),
+    ConfigModule.forRoot({
+      envFilePath: join(process.cwd(), '.env'),
+    }),
+    UsersModule,
+    LinksModule,
+  ],
 })
 export class AppModule {}
